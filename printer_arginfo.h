4ba4e90e035662e6d9fd42bc142a191faa96ad7a
brittain <yup@yup.com>
2026-01-29 17:24:53 +0000

    Revert "phpsdk_deps reduex"
    This reverts commit e35a802fcda626e8053769d4c0ec653ae9ee9d5f.

--- printer_arginfo.h
+++ printer_arginfo.h
@@ -216,7 +216,7 @@ ZEND_END_ARG_INFO()
 /* @return bool */
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_printer_draw_image, 0, 4, _IS_BOOL, 0)
     ZEND_ARG_INFO(0, connection) // Mixed (was IS_RESOURCE)
-    ZEND_ARG_OBJ_INFO(0, image_zval, GdImage, 0)
+    ZEND_ARG_TYPE_INFO(0, filename, IS_STRING, 0)
     ZEND_ARG_TYPE_INFO(0, x, IS_LONG, 0)
     ZEND_ARG_TYPE_INFO(0, y, IS_LONG, 0)
     ZEND_ARG_TYPE_INFO(0, width, IS_LONG, 1)
