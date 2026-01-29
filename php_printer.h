1de1e299a75acd2f1cbb110fbcc0c66a9178a164
brittain <yup@yup.com>
2026-01-29 17:24:58 +0000

    Revert "phpsdk_deps reduex"
    This reverts commit 9a4f6a61cb6425f679f67d65de17bbbe4e67007d.

--- php_printer.h
+++ php_printer.h
@@ -34,40 +34,32 @@ extern zend_module_entry printer_module_entry;
 
 PHP_MINIT_FUNCTION(printer)
 {
-    // Your existing init code here (e.g., registering constants or INI entries)
+   zval func_name, retval, params[2];
+   ZVAL_STRING(&func_name, "imagecreatetruecolor");
 
-    // New: Detect GD resource type
-    zval func_name, retval, params[2];
-    ZVAL_STRING(&func_name, "imagecreatetruecolor");
-    ZVAL_LONG(&params[0], 1);  // width
-    ZVAL_LONG(&params[1], 1);  // height
+   ZVAL_LONG(&params[0], 1);  // width
+   ZVAL_LONG(&params[1], 1);  // height
 
-    if (call_user_function(NULL, NULL, &func_name, &retval, 2, params) == SUCCESS &&
-        Z_TYPE(retval) == IS_RESOURCE) {
+   if (call_user_function(NULL, NULL, &func_name, &retval, 2, params) == SUCCESS
+       && Z_TYPE(retval) == IS_RESOURCE) {
 
-        le_gd = Z_RES(retval)->type;  // Save type ID (PHP 7/8 compatible)
+       le_gd = Z_RESVAL(retval)->type;  // Save the resource type ID
 
-        // Clean up temp image
-        zval destroy_func, destroy_params[1], destroy_retval;
-        ZVAL_STRING(&destroy_func, "imagedestroy");
-        ZVAL_COPY(&destroy_params[0], &retval);
-        call_user_function(NULL, NULL, &destroy_func, &destroy_retval, 1, destroy_params);
+       // Clean up the temporary image
+       zval destroy_func, destroy_params[1], destroy_retval;
+       ZVAL_STRING(&destroy_func, "imagedestroy");
+       ZVAL_COPY_VALUE(&destroy_params[0], &retval);
+       call_user_function(NULL, NULL, &destroy_func, &destroy_retval, 1, destroy_params);
 
-        zval_ptr_dtor(&destroy_func);
-        zval_ptr_dtor(&destroy_retval);
-    } else {
-        le_gd = -1;  // GD not available
-    }
+       zval_ptr_dtor(&destroy_func);
+       zval_ptr_dtor(&destroy_retval);
+   }
 
-    zval_ptr_dtor(&func_name);
-    zval_ptr_dtor(&retval);
-
-    if (le_gd == -1) {
-        php_error_docref(NULL, E_NOTICE, "GD extension not loaded - GD features in printer disabled");
-    }
-
-    // Your existing return (usually SUCCESS)
-    return SUCCESS;
+   zval_ptr_dtor(&func_name);
+   zval_ptr_dtor(&retval);
+}
+if (le_gd == -1) {
+   php_printf("Warning: GD extension not loaded - printer GD features disabled\n");
 }
 
 
